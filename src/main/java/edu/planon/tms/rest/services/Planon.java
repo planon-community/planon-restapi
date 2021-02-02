55e8b57083efb6b3fc7c6635b4367c8ecd05f6d9
Douglas Plumley <douglas.plumley@dartmouth.edu>
2021-02-02 16:40:55 -0500

    Setup working PUT method, needs testing.  Setup Delete method

--- src/main/java/edu/planon/tms/rest/services/Planon.java
+++ src/main/java/edu/planon/tms/rest/services/Planon.java
@@ -1,8 +1,11 @@
 package edu.planon.tms.rest.services;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,7 +43,7 @@ public class Planon {
     @GET
     @Path("/asset/{id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getAssetDetail(@PathParam("id") Integer id) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
+    public Response getAssetResponse(@PathParam("id") Integer id) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
         LOG.info("getting asset detail");
         JsonObject jsonObject = getBO("BaseAsset", id);
 
@@ -50,40 +53,35 @@ public class Planon {
     @POST
     @Path("/asset")
     @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response createOrUpdateAssetDetail(JsonObject request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
+    public Response createAssetResponse(Asset request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
         LOG.info("creating or updating asset detail");
-        JsonObject response = new JsonObject();
+
         Integer id = createBO("UsrAsset", request);
-        response.addProperty("id", id);
 
-        return Response.status(200).entity(response.toString()).build();
+        return Response.status(201).header("Location", "/asset/" + id).build();
     }
 
-    private Integer createBO(String type, JsonObject request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
-            IPnESBusinessObject genericBO = BusinessObject.create(type);
-            genericBO.getStringReferenceField("PropertyRef").setValue(request.get("property_ref"));
-            genericBO.getStringReferenceField("ItemGroupRef").setValue(request.get("item_group_ref"));
-            genericBO.getBooleanField("IsSimple").setValue(request.get("is_simple"));
-            genericBO.getBooleanField("IsArchived").setValue(request.get("is_archived"));
-            genericBO = genericBO.executeSave();
-            
-            LOG.info("Asset created");
-            
-            return genericBO.getPrimaryKey();
+    @PUT
+    @Path("/asset/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public Response updateAssetResponse(@PathParam("id") Integer id, Asset request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
+        LOG.info("creating or updating asset detail");
+
+        updateBO("UsrAsset", id, request);
+
+        return Response.status(200).build();
     }
 
-    private void updateAsset(Integer primaryKeyValue, Integer propertyValue, Integer groupValue, Boolean isSimple, Boolean isArchive) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
-            IPnESBusinessObject assetBO = BusinessObject.read("UsrAsset", primaryKeyValue);
-            assetBO.getStringReferenceField("PropertyRef").setValue(propertyValue);
-            assetBO.getStringReferenceField("ItemGroupRef").setValue(groupValue);
-            assetBO.getBooleanField("IsSimple").setValue(isSimple);
-            assetBO.getBooleanField("IsArchived").setValue(isArchive);
-            assetBO = assetBO.executeSave();
-            
-            LOG.info("Asset Updated");
-    }
+    @DELETE
+    @Path("/asset/{id}")
+    public Response deleteAssetResponse(@PathParam("id") Integer id) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException{
+        LOG.info("creating or updating asset detail");
 
+        deleteBO("UsrAsset", id);
+
+        return Response.status(200).build();
+    }
+    
     // TODO: Create a generic method to retrieve a BO that can be JSON serialized
     private JsonObject getBO(String type, Integer primaryKeyValue) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
         IPnESBusinessObject genericBO = BusinessObject.read(type, primaryKeyValue);
@@ -96,5 +94,38 @@ public class Planon {
 
         LOG.info("Getting asset");
         return response;
+    }
+
+    private Integer createBO(String type, Asset request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
+        LOG.info("Creating business object of type " + type);
+        IPnESBusinessObject genericBO = BusinessObject.create(type);
+
+        genericBO.getStringReferenceField("PropertyRef").setValue(request.getPropertyRef());
+        genericBO.getStringReferenceField("ItemGroupRef").setValue(request.getItemGroupRef());
+        genericBO.getBooleanField("IsSimple").setValue(request.getIsSimple());
+        genericBO.getBooleanField("IsArchived").setValue(request.getIsArchived());
+        genericBO = genericBO.executeSave();
+        
+        LOG.info("Business object created");
+        
+        return genericBO.getPrimaryKey();
+    }
+
+    private void updateBO(String type, Integer id, Asset request) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
+            LOG.info("Updating business object of type " + type);
+            IPnESBusinessObject assetBO = BusinessObject.read(type, id);
+
+            assetBO.getStringReferenceField("PropertyRef").setValue(request.getPropertyRef());
+            assetBO.getStringReferenceField("ItemGroupRef").setValue(request.getItemGroupRef());
+            assetBO.getBooleanField("IsSimple").setValue(request.getIsSimple());
+            assetBO.getBooleanField("IsArchived").setValue(request.getIsArchived());
+            assetBO = assetBO.executeSave();
+            
+            LOG.info("Asset Updated");
+    }
+
+    private Boolean deleteBO(String type, Integer id) throws PnESBusinessException, PnESActionNotFoundException, PnESFieldNotFoundException {
+        LOG.info("Deleting business object of type " + type);
+        return BusinessObject.delete(type, id);
     }
 }
