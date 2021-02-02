48fea15e41ffc791fa41cc4b6a54475397dd8eb7
Douglas Plumley <douglas.plumley@dartmouth.edu>
2021-02-02 16:40:23 -0500

    Used VS Code to generate getters/setters

--- src/main/java/edu/planon/tms/rest/dto/Asset.java
+++ src/main/java/edu/planon/tms/rest/dto/Asset.java
@@ -3,49 +3,54 @@ package edu.planon.tms.rest.dto;
 @SuppressWarnings("PMD")
 public class Asset {
 
-    private int primaryKey;
-    private Integer itemGroupRef;
-    private Integer properytyRef;
-    private boolean isSimple;
-    private boolean isArchived;
+    private int Syscode;
+    private String Code;
+    private Integer ItemGroupRef;
+    private Integer PropertyRef;
+    private boolean IsSimple;
+    private boolean IsArchived;
+
+    public int getSyscode() {
+        return Syscode;
+    }
 
     public Integer getItemGroupRef() {
-        return itemGroupRef;
+        return ItemGroupRef;
     }
 
     public void setItemGroupRef(Integer itemGroupRef) {
-        this.itemGroupRef = itemGroupRef;
+        ItemGroupRef = itemGroupRef;
     }
 
-    public Integer getProperytyRef() {
-        return properytyRef;
+    public Integer getPropertyRef() {
+        return PropertyRef;
     }
 
-    public void setProperytyRef(Integer properytyRef) {
-        this.properytyRef = properytyRef;
+    public void setPropertyRef(Integer propertyRef) {
+        PropertyRef = propertyRef;
     }
 
-    public boolean getIsArchived() {
-        return isArchived;
-    }
-    
     public boolean getIsSimple() {
-        return isSimple;
+        return IsSimple;
     }
 
     public void setIsSimple(boolean isSimple) {
-        this.isSimple = isSimple;
-    }
-    
-    public void setIsArchived(boolean isArchived) {
-        this.isArchived = isArchived;
-    }
-    
-    public int getPrimaryKey() {
-        return primaryKey;
+        IsSimple = isSimple;
     }
 
-    public void setPrimaryKey(int primaryKey) {
-        this.primaryKey = primaryKey;
+    public boolean getIsArchived() {
+        return IsArchived;
+    }
+
+    public void setIsArchived(boolean isArchived) {
+        IsArchived = isArchived;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
+    public void setCode(String code) {
+        Code = code;
     }
 }
